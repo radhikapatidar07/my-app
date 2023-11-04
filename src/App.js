5dcaeb6387b79c6faa754d9cf2294684b543a8b5
vaibhavpatidar522 <vaibhavpatidar522@gmail.com>
2023-11-04 13:13:04 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,36 +1,69 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import User from "./User";
+import Table from "react-bootstrap/Table";
 
 function App() {
   const student = [
-    { name: "Radhika", email: "radhika@gmail.com", contact: 444 },
-    { name: "Ram", email: "ram@gmail.com", contact: 111 },
-    { name: "Radha", email: "radha@gmail.com", contact: 222 },
-    { name: "Radhi", email: "radhi@gmail.com", contact: 333 },
+    {
+      name: "Radhika",
+      email: "radhika@gmail.com",
+      contact: 444,
+      address: [{ Hn: 11, country: "India", street: "mark" }],
+    },
+    {
+      name: "Ram",
+      email: "ram@gmail.com",
+      contact: 111,
+      address: [{ Hn: 22, country: "Nepal", street: "jon" }],
+    },
+    {
+      name: "Radha",
+      email: "radha@gmail.com",
+      contact: 222,
+      address: [{ Hn: 33, country: "China", street: "gark" }],
+    },
+    {
+      name: "Radhi",
+      email: "radhi@gmail.com",
+      contact: 333,
+      address: [{ Hn: 44, country: "US", street: "neml" }],
+    },
   ];
   return (
     <div className="App">
       <h1>Handle Array with list</h1>
-      <table>
+      <Table>
         <tbody>
           <tr>
+            <td>S.No</td>
             <td>Name</td>
             <td>email</td>
             <td>Contact</td>
           </tr>
-          {User.map((item, i) =>
-            item.contact === "111" ? (
-              <tr>
-                <td>{item.name}</td>
-                <td>{item.email}</td>
-                <td>{item.contact}</td>
-              </tr>
-            ) : null
-          )}
         </tbody>
-      </table>
+
+        {student.map((item, i) =>
+          (<tr key={i}>
+            <td>{i}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.contact}</td>
+          </tr>)(
+            <Table>
+              <tbody>
+                {item.address.map((data) => (
+                  <tr>
+                    <td>{data.Hn}</td>
+                    <td>{data.street}</td>
+                    <td>{data.contact}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )
+        )}
+      </Table>
     </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -1,12 +1,12 @@
-import React from "react";
-function User(props) {
-  return (
-    <div>
-      <span> {props.data.name} </span>
-      <span> {props.data.email} </span>
-      <span> {props.data.contact} </span>
-    </div>
-  );
-}
+// import React from "react";
+// function User(props) {
+//   return (
+//     <div>
+//       <span> {props.data.name} </span>
+//       <span> {props.data.email} </span>
+//       <span> {props.data.contact} </span>
+//     </div>
+//   );
+// }
 
-export default User;
+// export default User;
