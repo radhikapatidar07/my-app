e76f744c8e91399376188b12c28d40120cbf1be9
vaibhavpatidar522 <vaibhavpatidar522@gmail.com>
2023-11-21 11:46:10 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
-import User from "./User";
-import React, { useRef } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import About from "./component/About";
 
 function App() {
-  let inputRef = useRef(null);
-  function updateInput() {
-    inputRef.current.value = 1000;
-    inputRef.current.focus();
-    inputRef.current.style.color = "red";
-  }
   return (
     <div className="App">
-      <h1>Use Ref in React</h1>
-      <User ref={inputRef}></User>
-      <button onClick={updateInput}>Update Input Box</button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
