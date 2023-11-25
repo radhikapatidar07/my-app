bedb575610189948ae5b356cb7f950d84806bda8
radhikapatidar07 <radhikachawda777@gmail.com>
2023-11-25 15:21:37 +0530

    fitAPI

--- src/App.js
+++ src/App.js
@@ -1,32 +1,119 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-import User from "./User";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  let users = [
-    { id: 1, name: "Radhika", email: "radhika@test.com" },
-    { id: 2, name: "Pooja", email: "pooja@test.com" },
-    { id: 3, name: "Sam", email: "sam@test.com" },
-    { id: 4, name: "Neetu", email: "neetu@test.com" },
-    { id: 5, name: "Mina", email: "mina@test.com" },
-  ];
+  const [users, setUser] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  function getUser() {
+    fetch("http://localhost:3000/todo").then((result) => {
+      result.json().then((resp) => {
+        // console.warn(resp);
+        setUser(resp);
+        setName(resp[0].name);
+        setName(resp[0].email);
+        setName(resp[0].mobile);
+        setUserId(resp[0].id);
+      });
+    });
+  }
+  // console.warn(users);
+
+  function deleteUser(id) {
+    fetch(`http://localhost:3000/todo/${id}`, {
+      method: "DELETE",
+    }).then((result) => {
+      result.json().then((resp) => {
+        // console.warn(resp);
+        getUser();
+      });
+    });
+  }
+
+  function selectUser(id) {
+    console.warn("Function Called", users[id - 1]);
+    let item = users[id - 1];
+    setName(item.name);
+    setEmail(item.email);
+    setMobile(item.mobile);
+    setUserId(item.id);
+  }
+
+  function updateUser() {
+    let items = { name, email, mobile, userId };
+    fetch(`http://localhost:3000/todo/${userId}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(items),
+    }).then((result) => {
+      result.json().then((resp) => {
+        // console.warn(resp);
+        getUser();
+      });
+    });
+  }
+
   return (
     <div className="App">
-      <Router>
-        <h1>React Dhynamic Routing</h1>
-        {users.map((item) => (
-          <div>
-            <Link to={"/user/" + item.id + "/" + item.name}>
-              <h3>{item.name}</h3>
-            </Link>
-          </div>
-        ))}
-        <Routes>
-          <Route path="/user/:id/:name" element={<User />}></Route>
-        </Routes>
-      </Router>
+      <h1>Get API call</h1>
+      <table border="1" style={{ float: "left" }}>
+        <tbody>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Mobile</td>
+            <td>Operations</td>
+          </tr>
+          {users.map((item, i) => (
+            <tr key={i}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.mobile}</td>
+              <td>
+                <button onClick={() => deleteUser(item.id)}>Delete</button>
+                <button onClick={() => selectUser(item.id)}>Update</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          value={mobile}
+          onChange={(e) => setMobile(e.target.value)}
+        />
+        <br />
+        <br />
+        <button onClick={updateUser}>Update User</button>
+      </div>
     </div>
   );
 }
--- src/User.js
+++ src/User.js
@@ -1,11 +1,17 @@
-import { withRouter } from "react-router-dom";
-function User(prop) {
-  console.warn(prop.match.params.id);
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function User(props) {
+  const navigate = useNavigate();
+
+  console.warn(props.params.id);
+
   return (
     <div>
-      <h1>Hi this is user no {prop.match.params.id}</h1>
-      <h1>Hi this is {prop.match.params.name}</h1>
+      <h1>Hi this is user no {props.params.id}</h1>
+      <h1>Hi this is {props.params.name}</h1>
     </div>
   );
 }
-export default withRouter(User);
+
+export default User;
